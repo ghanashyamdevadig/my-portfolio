eccfef81d268b34ad40e3b17a67347cf5422e42e
Shyam <shyam@Shyams-MacBook-Air.local>
2026-03-13 16:17:38 +0530

    package changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio/",
 };
 
 module.exports = nextConfig;
