05488743fac7b940b9005853adb42e9a28c77fb1
Shyam <shyam@Shyams-MacBook-Air.local>
2026-03-13 16:34:38 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
-  basePath: "/my-portfolio",
-  assetPrefix: "/my-portfolio/",
+  basePath: isProd ? "/my-portfolio" : "",
+  assetPrefix: isProd ? "/my-portfolio/" : "",
 };
 
 module.exports = nextConfig;
