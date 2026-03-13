180451281231ecb503cf61c077bd138babbb3c31
Shyam <shyam@Shyams-MacBook-Air.local>
2026-03-13 16:59:00 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const isProd = process.env.NODE_ENV === "production";
 const repo = "my-portfolio";
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
   images: {
     unoptimized: true,
   },
